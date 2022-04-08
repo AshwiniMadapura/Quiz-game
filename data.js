08137801abfa2c7957dcf3b8985ed56fe5e37ed3
AshwiniMadapura <ashwinidivakar8@gmail.com>
2022-04-08 09:21:58 +0100

    questions display using data

--- data.js
+++ data.js
@@ -6,7 +6,8 @@ export const qestionArray=[
         answerB:"Copenhagen",
         answerC:"Helsingor",
         answerD:"Gothenberg",
-        correctAnswer:"copenhagen"
+        correctAnswer:"Copenhagen",
+        img:"./images/denmark.jpg",
         
     },
     {
@@ -14,28 +15,51 @@ export const qestionArray=[
         answerA:"Bone",
         answerB:"Tendon",
         answerC:"Tooth Enamel",
-        answerD:"Muscle"
+        answerD:"Muscle",
+        img:"./images/hardest.jpg",
     },
     {
         question:"Grand Central Terminal, Park Avenue, New York is the world's?",
         answerA:"largest railway station",
         answerB:"highest railway station",
         answerC:"longest railway station",
-        answerD:"None of the above"
+        answerD:"None of the above",
+        img:"./images/parkAve.jpg",
     },
     {
         question:"Entomology is the science that studies?",
         answerA:"Behavior of human beings",
         answerB:"Insects",
         answerC:"The origin and history of technical and scientific terms",
-        answerD:"The formation of rocks"
+        answerD:"The formation of rocks",
+        img:"./images/study.jpg",
     },
     {
         question:"Garampani sanctuary is located at",
         answerA:"Junagarh, Gujarat",
         answerB:"Diphu, Assam",
         answerC:"Kohima, Nagaland",
-        answerD:"Gangtok, Sikkim"
+        answerD:"Gangtok, Sikkim",
+        img:"./images/grampani.jpg",
+    },
+    {
+        question:"Epsom (England) is the place associated with",
+        answerA:"Horse racing",
+        answerB:"Polo",
+        answerC:"Shooting",
+        answerD:"Snooker",
+        img:"./images/grampani.jpg",
+    },
+    {
+        question:"Durand Cup is associated with the game of",
+        answerA:"Cricket",
+        answerB:"Football",
+        answerC:"Hockey",
+        answerD:"Volleyball",
+        img:"./images/grampani.jpg",
     },
 
 ]
+
+
+export const correctAnsArray =["Copenhagen","Tooth Enamel"]
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import {qestionArray} from "./data.js";
+import {qestionArray,correctAnsArray} from "./data.js";
+
 
 
 const optionButton=document.querySelectorAll(".options")
@@ -16,32 +17,55 @@ const optionButton=document.querySelectorAll(".options")
 // }
 
 function ansButton(){
-    console.log(qestionArray[0].correctAnswer)
-    console.log(qestionArray[0].answerA)
+    // console.log(qestionArray[0].correctAnswer)
     
-    
-    if(qestionArray[0].correctAnswer==qestionArray[0].answerA){
-        console.log(qestionArray[0].correctAnswer,qestionArray[0].answerA)
-        console.log("insideIf?");
-        document.getElementById("output").innerHTML="correct"
-    }
-    else{
-        console.log("insideElse?");
-        document.getElementById("output").innerHTML="incorrect"
-    }
+    // console.log(typeof(this.value))
+    // console.log(Object.keys(qestionArray[0])[3])
 
-    if(qestionArray[0].correctAnswer==qestionArray[0].answerB){
-        console.log("inside ansB")
-        document.getElementById("output").innerHTML="correct"
-    }else{
-        console.log("insideBElse?");
-         document.getElementById("output").innerHTML="incorrect"
+    // let chosenAns=this.value;
+    let chosenAns=""
+    if (this.value == "answerA"){
+        chosenAns = Object.values(qestionArray[0])[1]
+    }
+    else if (this.value == "answerB"){
+        chosenAns = Object.values(qestionArray[0])[2]
+    }
+    else if (this.value == "answerC"){
+        chosenAns = Object.values(qestionArray[0])[3]
+    }
+    else if (this.value == "answerD"){
+        chosenAns = Object.values(qestionArray[0])[4]
     }
 
+    if (chosenAns == correctAnsArray[0]){
+            document.getElementById("output").innerHTML="correct"    
+        }
+        else{
+            document.getElementById("output").innerHTML="incorrect"    
+        }
     
-
-
     
+    // if(qestionArray[0].correctAnswer==qestionArray[0].this.value){
+    //     console.log(qestionArray[0].correctAnswer,qestionArray[0].answerA)
+    //     console.log("insideIf?");
+    //     document.getElementById("output").innerHTML="correct"
+    // }
+    // // else if(qestionArray[0].correctAnswer==qestionArray[0].answerB){
+    // //         console.log("inside ansB")
+    // //         document.getElementById("output").innerHTML="correct"
+    // //     }
+    // else{
+    //     console.log("insideElse?");
+    //     document.getElementById("output").innerHTML="incorrect"
+    // }
+
+    // if(qestionArray[0].correctAnswer==qestionArray[0].answerB){
+    //     console.log("inside ansB")
+    //     document.getElementById("output").innerHTML="correct"
+    // }else{
+    //     console.log("insideBElse?");
+    //      document.getElementById("output").innerHTML="incorrect"
+    // }   
 }
 let count=0;
 document.getElementById("nextQuestion").addEventListener("click",nextButton);
@@ -55,11 +79,35 @@ function nextButton(){
     document.getElementById("2").innerHTML=qestionArray[count].answerB;
     document.getElementById("3").innerHTML=qestionArray[count].answerC;
     document.getElementById("4").innerHTML=qestionArray[count].answerD;
-}
 
+    var x = document.createElement("IMG");
+        // x.setAttribute("src", "./images/denmark.jpg");
+        x.setAttribute("src", qestionArray[count].img);
+        console.log(count+"inside image");
+        x.setAttribute("width", "304");
+        x.setAttribute("height", "228");
+        
+        // x.setAttribute("alt", "The Pulpit Rock");
+        document.body.appendChild(x);
+}
+    if(count==0){
     console.log(qestionArray[count].question)
     document.getElementById("input").value=qestionArray[count].question;
     document.getElementById("1").innerHTML=qestionArray[count].answerA;
     document.getElementById("2").innerHTML=qestionArray[count].answerB;
     document.getElementById("3").innerHTML=qestionArray[count].answerC;
     document.getElementById("4").innerHTML=qestionArray[count].answerD;
+    // document.getElementById("imgsec").value=qestionArray[count].img;
+
+    // function myFunction() {
+        var x = document.createElement("IMG");
+        // x.setAttribute("src", "./images/denmark.jpg");
+        x.setAttribute("src", qestionArray[count].img);
+        console.log(count+"inside image");
+        x.setAttribute("width", "304");
+        x.setAttribute("height", "228");
+        
+        // x.setAttribute("alt", "The Pulpit Rock");
+        document.body.appendChild(x);
+    //   }
+    }
